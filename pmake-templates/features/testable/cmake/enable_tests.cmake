ccf52cdb46785d7d0de05ad01aae70eb511467e1
nyyakko <nyakonyns@gmail.com>
2024-04-04 20:47:04 -0300

    this is not needed since cmakepresets logs the option

--- pmake-templates/features/testable/cmake/enable_tests.cmake
+++ pmake-templates/features/testable/cmake/enable_tests.cmake
@@ -1,7 +1,5 @@
 function(enable_tests)
 
-    message(STATUS "[${PROJECT_NAME}] project will be building the test suit.")
-
     if (NOT ${CMAKE_HOST_SYSTEM_NAME} MATCHES "Windows")
         message(STATUS "[${PROJECT_NAME}] running on ${CMAKE_HOST_SYSTEM_NAME}, sanitizers for tests are enabled.")
         set(!PROJECT!_TestsLinkerOptions ${!PROJECT!_TestsLinkerOptions} -fsanitize=undefined,leak,address)
