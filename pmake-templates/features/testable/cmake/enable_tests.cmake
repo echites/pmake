7f7b11df5b537bf3c24b11cb2b545ee8a9f015ea
nyyakko <nyakonyns@gmail.com>
2024-05-06 19:13:24 -0400

    update templates

--- pmake-templates/features/testable/cmake/enable_tests.cmake
+++ pmake-templates/features/testable/cmake/enable_tests.cmake
@@ -1,5 +1,7 @@
 function(enable_tests PROJECT)
 
+    include(GoogleTest)
+
     if (NOT ${CMAKE_HOST_SYSTEM_NAME} MATCHES "Windows")
         message(STATUS "[${PROJECT}] running on ${CMAKE_HOST_SYSTEM_NAME}, sanitizers for tests are enabled.")
         set(!PROJECT!_TestsLinkerOptions ${!PROJECT!_TestsLinkerOptions} -fsanitize=undefined,leak,address)
@@ -8,15 +10,16 @@ function(enable_tests PROJECT)
     endif()
 
     set(!PROJECT!_TestsCompilerOptions ${!PROJECT!_TestsCompilerOptions} ${!PROJECT!_CompilerOptions})
-
-    include(GoogleTest)
+    set(!PROJECT!_TestsLinkerOptions ${!PROJECT!_TestsLinkerOptions} ${!PROJECT!_LinkerOptions})
 
     CPMAddPackage(
         NAME googletest
         GITHUB_REPOSITORY google/googletest
         GIT_TAG release-1.12.1
         VERSION 1.12.1
-        OPTIONS "INSTALL_GTEST OFF" "gtest_force_shared_crt"
+        OPTIONS
+            "INSTALL_GTEST OFF"
+            "gtest_force_shared_crt"
     )
 
     enable_testing()
--- pmake-templates/features/testable/runtests.py
+++ pmake-templates/features/testable/runtests.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('ctest --test-dir build')
+os.system('ctest --test-dir build/tests')
